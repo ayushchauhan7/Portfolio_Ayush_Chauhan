2474964f5c289e08b28a404017422e3e5c3c703f
Ayush Chauhan <ayush.22b1541073@abes.ac.in>
2025-08-01 23:06:47 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from "./sections/Navbar.jsx";
+import Hero from "./sections/Hero.jsx";
+import ServiceSummary from "./sections/ServiceSummary.jsx";
 
 const App = () => {
 	return (
 		<div className='relative w-screen min-h-screen overflow-x-auto'>
 			<Navbar />
+			<Hero />
+			<ServiceSummary />
 		</div>
 	);
 };
